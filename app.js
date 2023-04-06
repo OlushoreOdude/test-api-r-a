54790d66488308af2e675b1090f39b1b05d867c4
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-04-06 21:32:01 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const PORT = process.env.PORT || 3500;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("test-hello-oo");
+});
+
+app.get("/hat", (req, res) => {
+  res.send("hats hats ");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
